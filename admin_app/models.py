429c3e281468ddddfa0a335893405659aa08bed5
jyoth-irmayi <jyothirmayirani289@gmail.com>
2025-02-07 19:42:01 +0530

    cartitem model modified

--- admin_app/models.py
+++ admin_app/models.py
@@ -142,10 +142,11 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(
-        Cart,
-        on_delete=models.CASCADE,
-        related_name='cart_items'
-    )
+    Cart,
+    on_delete=models.CASCADE,
+    related_name='cart_items',
+    null=True, blank=True  # ✅ Temporarily allow null values
+)
     variant = models.ForeignKey(
         Variant,
         on_delete=models.CASCADE,
